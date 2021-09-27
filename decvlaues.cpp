7148efebd1694dca83f9698b922851216278915e
randyayittah <randy.ayittah@turntabl.io>
2021-09-27 07:45:59 +0000

    changes on decfile.cpp

--- decvlaues.cpp
+++ decvlaues.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main(){
-	std::out <<"0xf3f2 represent" << int(0xf3f2) <<"\n";
-	std::cout <<"0437 represent" << int(0437) <<"\n";
-	std::cout <<"a represent" << int("a") i<<"\n";
+	char a = 'a';
+	std::cout <<"0xf3f2 represent " << int(0xf3f2) <<"\n";
+	std::cout <<"0437 represent " << int(0437) <<"\n";
+	std::cout <<"a represent " << (int)a <<"\n";
 
 return 0;
 
